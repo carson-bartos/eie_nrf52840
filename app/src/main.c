873a296e1fbe59c3f55ed51d53c1826e51582f0e
Carson Bartos <csbartos@icloud.com>
2025-10-16 19:55:35 -0600

    API Button press LED on

--- app/src/main.c
+++ app/src/main.c
@@ -8,39 +8,26 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-#define SLEEP_TIME_MS 1000
+#include "BTN.h"
+#include "LED.h"
 
-#define SW0_NODE DT_ALIAS(sw0)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-
-static struct gpio_callback button_isr_data;
-
-void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins){
-  printk("STOP PRESSING THE BUTTON!!!!\n");
-}
+#define SLEEP_MS 1
 
 int main(void) {
-  int ret;
+ if (0 < BTN_init()){
+  return 0;
+ }
 
-  if (!gpio_is_ready_dt(&button)){
-    return 0;
-  }
-
-  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-  if (0 > ret) {
-    return 0;
-  }
-
-  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-  if (0 > ret){
-    return 0;
-  }
-
-  gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
-  gpio_add_callback(button.port, &button_isr_data);
+ if(0 > LED_init()){
+  return 0;
+ }
 
   while(1) {
-    
+    if (BTN_check_clear_pressed(BTN0)){
+      LED_toggle(LED0);
+      printk("Button 0 pressed!\n");
+    }
+    k_msleep(SLEEP_MS);
   }
 	return 0;
 }
